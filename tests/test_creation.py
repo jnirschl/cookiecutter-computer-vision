b5eb05d59e4c123990ba68b4c3a5e596ee8279db
Emily Miller <ejm714@gmail.com>
2021-03-05 10:41:53 -0800

    fix missing comma

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -90,7 +90,7 @@ def verify_files(root, config):
         "docs/getting-started.rst",
         "docs/index.rst",
         "docs/make.bat",
-        "ethics.md"
+        "ethics.md",
         "notebooks/.gitkeep",
         "notebooks/.nbautoexport",
         "references/.gitkeep",
