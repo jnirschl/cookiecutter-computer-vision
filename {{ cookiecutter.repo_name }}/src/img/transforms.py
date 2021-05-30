3a3ff948ee4561deb3400dd79d83453cae41a635
Jeff Nirschl <jnirschl@stanford.edu>
2021-05-29 19:02:03 -0700

    Update image transforms.py and test_train_model.py

--- {{ cookiecutter.repo_name }}/src/img/transforms.py
+++ {{ cookiecutter.repo_name }}/src/img/transforms.py
@@ -1,31 +1,93 @@
 #!/usr/bin/env python3
 
+import os
+import click
+import logging
+from pathlib import Path
+
 import cv2
 import numpy as np
 import pandas as pd
 
 # Specify opencv optimization
+from dotenv import find_dotenv, load_dotenv
+
 cv2.setUseOptimized(True)
 
 
-def mean_image(img_array, img_shape=(28, 28, 1)):
+@click.command()
+@click.option(
+    "--grayscale", default=True, help="Switch to read images as 8-bit grayscale."
+)
+@click.argument(
+    "mapfile",
+    default=Path("./data/interim/mapfile.csv").resolve(),
+    type=click.Path(exists=True),
+)
+# @click.argument(
+#     "output_dir",
+#     default=Path("./data/interim").resolve(),
+#     type=click.Path(),
+# )
+def mean_image(mapfile, img_shape=None, grayscale=False):
     """Accept images as numpy array with images separated by rows
     and columns indicating pixel values"""
-    assert type(img_array) is type(pd.DataFrame()), TypeError
+    assert type(mapfile) is str, TypeError(f"MAPFILE must be type STR")
+    assert img_shape is None or (img_shape is tuple and len(img_shape) == 3), TypeError(
+        f"IMG_SHAPE must be tuple with a length of 3"
+    )
 
-    # pre-allocate mean_img
+    # set image flag
+    if grayscale:
+        FORMAT = cv2.IMREAD_GRAYSCALE
+    else:
+        FORMAT = cv2.IMREAD_COLOR
+
+    # read mapfile
+    mapfile_df = pd.read_csv(mapfile, sep=",", header=0, index_col=0)
+
+    # pre-allocate mean image
+    if img_shape is None:
+        # use shape of first image if none given
+        img_shape = cv2.imread(mapfile_df["filename"][0], FORMAT).shape
+
     mean_img = np.zeros(img_shape, dtype=np.float32)
 
     # process files
     print(f"Computing mean image...")
-    for file_count, idx in enumerate(range(img_array.shape[0])):
-        temp_img = np.reshape(img_array.iloc[idx].to_numpy(), img_shape)
-        mean_img = cv2.accumulate(temp_img.astype(dtype=np.float32), mean_img)
+    for idx, (filename, label) in mapfile_df.iterrows():
+        # print(f"{idx}\t{filename}\t{label}")
+        img = cv2.imread(filename, FORMAT)
 
-        if file_count % 10000 == 0:
-            print(f"\tProcessed {file_count:0d} images.")
+        # ensure image is valid
+        if img is None:
+            raise ValueError(f"Error loading image:\t{filename}")
+        else:
+            if idx % 1000 == 0:
+                print(f"Processed {idx} images.")
+
+            # accumulate
+            mean_img = cv2.accumulate(img.astype(dtype=np.float32), mean_img)
 
     # divide by n_images
-    mean_img = np.divide(mean_img, file_count + 1)
+    print(f"Processed {idx+1} images.")
+    mean_img = np.divide(mean_img, idx + 1)
 
-    return mean_img
+    # save image
+    output_filename = str(Path(mapfile).parent.joinpath("mean_image.png"))
+    cv2.imwrite(output_filename, mean_img)
+    return
+
+
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+
+    mean_image()
--- {{ cookiecutter.repo_name }}/src/tests/test_transforms.py
+++ {{ cookiecutter.repo_name }}/src/tests/test_transforms.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
+
 import numpy as np
 import pandas as pd
+import pytest
 
 from src.img import transforms
 
+@pytest.fixture
+def mapfile():
+    return "./src/tests/test_data/pytest_mapfile.csv"
 
-class TestTransforms:
-    def test_zero(self):
-        img_array = pd.DataFrame(np.zeros((10, 784), dtype=np.float32))
-        mean_image = transforms.mean_image(img_array)
-        assert np.mean(mean_image) == 0
 
-    def test_rand(self):
-        img_array = pd.DataFrame(np.random.randn(1000, 784), dtype=np.float32)
-        mean_image = transforms.mean_image(img_array)
-        assert np.subtract(np.mean(mean_image), 0) < 0.005
+class TestTransforms():
+    pass
