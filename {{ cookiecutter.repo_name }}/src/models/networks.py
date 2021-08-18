965bdf1027009d2a58cca9b7544f66e8714fedcd
Jeff Nirschl <jnirsch@gmail.com>
2021-08-18 14:12:07 -0700

    Reduce filters for each convolutional layer

--- {{ cookiecutter.repo_name }}/src/models/networks.py
+++ {{ cookiecutter.repo_name }}/src/models/networks.py
@@ -17,14 +17,14 @@ def simple_nn(input_shape=(28, 28), n_classes=10, debug=False):
         model = tf.keras.models.Sequential(
             [
                 tf.keras.layers.InputLayer(input_shape=input_shape),
-                tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+                tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
                 tf.keras.layers.MaxPooling2D(2, 2),
-                tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+                tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
                 tf.keras.layers.MaxPooling2D(2, 2),
-                tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+                tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
                 tf.keras.layers.MaxPooling2D(2, 2),
                 tf.keras.layers.Flatten(),
-                tf.keras.layers.Dense(512, activation="relu"),
+                tf.keras.layers.Dense(128, activation="relu"),
             ]
         )
 
