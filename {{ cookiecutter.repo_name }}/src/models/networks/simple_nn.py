Bulk add tweaks package nerve_morphometry. Includes: predict_model.py function to generate predictions on any size input fix error in calculation of train/val images in train_model.py add logging to train_callbacks.py minor updates to parameter names in simple_nn.py

{{ cookiecutter.repo_name }}/src/models/networks/simple_nn.py
@@ -3,10 +3,10 @@
 import tensorflow as tf
 
 
-def simple_nn(input_shape=(28, 28, 1), n_classes=10, debug=False):
+def simple_nn(input_shape=(28, 28, 1), n_classes=10, deterministic=False):
     """ """
 
-    if debug:
+    if deterministic:
         model = tf.keras.models.Sequential(
             [
                 tf.keras.layers.Flatten(input_shape=input_shape),

{{ cookiecutter.repo_name }}/src/models/predict_model.py
@@ -7,138 +7,75 @@ from dotenv import find_dotenv, load_dotenv
 import logging
 import numpy as np
 from pathlib import Path
-import pandas as pd
-import skimage.io as io
-
-# import cv2
-# import numpy as np
-# import pandas as pd
-# import skimage.io as io
 
 import tensorflow as tf
-from tensorflow.data import AUTOTUNE
 
+from MightyMosaic import MightyMosaic
 
 # load custom libraries from src
-from src.data import mapfile, load_data
-from src.img.tfreader import tf_imread, tf_imread_predict, tf_imreadpair, tf_dataset
-from src.img.augment import apply_transforms, tf_standardize, tf_normalize
 from src.data import load_params
-from src.models import networks
 
 # set tf warning options
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
 def predict(
-    mapfile_path,
+    input_dir,
     model_path,
     params_filepath="params.yaml",
     results_dir="./results",
-    debug=False,
+    file_ext="png",
+    overlap_factor=4,
 ):
-    """Accept filepaths to the mapfile, load model and predict ouputs."""
-    assert type(mapfile_path) is str, TypeError(f"MAPFILE must be type STR")
+    """Accept input directory and trained model, and save predicted ouput images."""
+
+    # resolve directories
+    input_dir = Path(input_dir).resolve()
+    model_path = Path(model_path).resolve()
+    results_dir = Path(results_dir).resolve()
+
+    assert input_dir.exists(), NotADirectoryError(f"{input_dir}")
+    assert model_path.exists(), NotADirectoryError(f"{model_path}")
 
     # start logger
     logger = logging.getLogger(__name__)
-
-    # read files
-    mapfile_df = load_data(mapfile_path, sep=",", header=0, index_col=0, dtype=str)
 
     # load model
     model = tf.keras.models.load_model(model_path)
 
     # load params
     params = load_params(params_filepath)
-    train_params = params["train_model"]
-    random_seed, target_size, n_classes, mean_img, std_img = (
-        params["random_seed"],
+    target_size, mean_img, std_img = (
         params["target_size"],
-        params["n_classes"],
         params["mean_img"],
         params["std_img"],
     )
-    batch_size, epochs = train_params["batch_size"], train_params["epochs"]
 
-    # create dataset using tf.data
-    data_records = [list(elem) for elem in mapfile_df.to_records(index=False)]
-    dataset = tf.data.Dataset.from_tensor_slices(data_records)
+    file_list = input_dir.glob("*." + file_ext.replace(".", ""))
+    for file in file_list:
+        temp_filepath = Path(file).resolve()
+        if temp_filepath.is_file():
+            img = cv2.imread(str(temp_filepath), cv2.IMREAD_COLOR)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img_std = np.divide(np.subtract(img / 255, mean_img), std_img)
 
-    # build tf.data pipeline
-    if params["segmentation"]:
-        dataset = dataset.map(
-            tf_imread_predict,
-            num_parallel_calls=AUTOTUNE,
-        )
-    else:
-        dataset = dataset.map(
-            tf_imread,
-            num_parallel_calls=AUTOTUNE,
-        )
+            mosaic = MightyMosaic.from_array(
+                img_std, target_size[0:2], overlap_factor=overlap_factor
+            )
+            prediction = mosaic.apply(model.predict, progress_bar=True, batch_size=32)
+            pred_img = prediction.get_fusion()[:, :, 1]
+            pred_img = cv2.normalize(pred_img, None, 0, 255, cv2.NORM_MINMAX)
 
-    # pre-processing
-    dataset = dataset.map(
-        lambda x: (tf_standardize(x, mean=mean_img, std=std_img)),
-        num_parallel_calls=AUTOTUNE,
-    )
+            out_filepath = str(results_dir.joinpath(file.name))
+            cv2.imwrite(out_filepath, pred_img)
 
-    # # pre-processing
-    # dataset = dataset.map(
-    #     lambda x, y: (
-    #         tf_standardize(x, mean=mean_img, std=std_img),
-    #         y,
-    #     ),
-    #     num_parallel_calls=AUTOTUNE,
-    # )
-
-    dataset = dataset.cache().batch(  # cache after mapping
-        batch_size=batch_size,
-        drop_remainder=True,
-        # num_parallel_calls=AUTOTUNE,
-        deterministic=debug,
-    )
-
-    # set output filepath
-    if not Path(results_dir).exists:
-        print("Directory not exists!")
-        return
-    
-    img_filepath = Path(results_dir).joinpath("test_image.png")
-
-    # test filepath
-    Path("nerve_0000.png").resolve()
-
-    test_img = dataset.take(60)
-    img_predict = model.predict(dataset)
-    for idx in range(mapfile_df.shape[0]):
-        temp_img = img_predict[idx]
-        # img_filepath = Path("./results").joinpath(f"test_image_{idx:03d}.png")
-        img_filepath = Path("./results").joinpath(
-            mapfile_df["filename"][idx].replace(".png", "_mask.png")
-        )
-        cv2.imwrite(str(img_filepath), (temp_img[:, :, 1] * 255).astype(np.uint8))
-        # print(f"min={np.min(img_predict[:])}\nmax={np.max(img_predict[:])}")
-
-    # # predict single image
-    # test_img = dataset.take(1)
-    # img_predict = model.predict(test_img)[0]
-    # print(f"min={np.min(img_predict[:])}\nmax={np.max(img_predict[:])}")
-    # img_predict = (img_predict[:,:,1] * 255).astype(np.uint8)
-    # cv2.imwrite(str(img_filepath), img_predict)
-    # # norm = np.zeros(img_predict.shape)
-    # # img_predict2 = cv2.normalize(img_predict, norm, 0, 255, cv2.NORM_MINMAX).astype(
-    # #     np.uint8
-    # # )
-    # # cv2.imwrite(str(img_filepath), img_predict2)
-    # # cv2.imwrite(str(img_filepath).replace("test", "orig"), (test_img * 255).astype(np.uint8))
     return True
 
 
 @click.command()
 @click.argument(
-    "mapfile_path",
-    default=Path("./data/processed/mapfile_df.csv").resolve(),
+    "input_dir",
+    default=Path("./data/raw/data").resolve(),
     type=click.Path(exists=True),
     # help="Filepath to the CSV with image filenames and class labels.",
 )
@@ -147,29 +84,28 @@ def predict(
     type=click.Path(exists=True),
 )
 @click.option("--params_filepath", "-p", default="params.yaml")
-# @click.option(
-#     "--results-dir",
-#     default=Path("./results").resolve(),
-#     type=click.Path(),
-# )
+@click.option("--file_ext", default="png")
+@click.option("--overlap_factor", default=8)
 @click.option(
-    "--debug",
-    is_flag=True,
-    help="Debug switch that turns off augmentation, shuffle, and makes runs deterministic.",
+    "--results-dir",
+    default=Path("./results").resolve(),
+    type=click.Path(),
 )
 def main(
-    mapfile_path,
+    input_dir,
     model_path,
     params_filepath="params.yaml",
     results_dir="./results",
-    debug=False,
+    file_ext="png",
+    overlap_factor=4,
 ):
     predict(
-        mapfile_path,
+        input_dir,
         model_path,
         params_filepath=params_filepath,
         results_dir=results_dir,
-        debug=debug,
+        file_ext=file_ext,
+        overlap_factor=overlap_factor,
     )
 
 

{{ cookiecutter.repo_name }}/src/models/train_callbacks.py
@@ -17,6 +17,11 @@ from src.data import load_params
 def set(params_filepath="params.yaml"):
     """Accept params_filepaths, loads params.yaml, and returns a list of tf callbacks."""
 
+    # start logger
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logger = logging.getLogger(__name__)
+
     # load params
     params = load_params(params_filepath)
     callback_params = params["train_model"]["callbacks"]
@@ -25,12 +30,13 @@ def set(params_filepath="params.yaml"):
     callback = []
     for elem in callback_params["callbacks"]:
         if elem.lower() in ["earlystopping"]:
+            logger.info(f"Callback: EarlyStopping")
             callback.append(
                 tf.keras.callbacks.EarlyStopping(
                     monitor=callback_params["monitor"],
                     min_delta=0,
                     patience=15,
-                    verbose=0,
+                    verbose=1,
                     mode=callback_params["mode"],
                     baseline=None,
                     restore_best_weights=False,
@@ -38,12 +44,13 @@ def set(params_filepath="params.yaml"):
             )
 
         if elem.lower() in ["reducelronplateau"]:
+            logger.info(f"Callback: ReduceLROnPlateau")
             callback.append(
                 tf.keras.callbacks.ReduceLROnPlateau(
                     monitor=callback_params["monitor"],
                     factor=0.1,
                     patience=5,
-                    verbose=0,
+                    verbose=1,
                     mode=callback_params["mode"],
                     min_delta=0.0001,
                     cooldown=0,

{{ cookiecutter.repo_name }}/src/models/train_model.py
@@ -31,11 +31,10 @@ def train(
     mapfile_path,
     cv_idx_path,
     params_filepath="params.yaml",
-    model_dir="./models",
+    model_dir="./models/dev",
     model_name="model",
     results_dir="./results",
-    metrics_filepath="metrics.json",
-    debug=False,
+    metrics_file="metrics.json",
 ):
     """Accept filepaths to the mapfile and train-dev split, train mode, and return
     training history."""
@@ -44,28 +43,26 @@ def train(
     # start logger
     logger = logging.getLogger(__name__)
 
-    # read files
-    mapfile_df, cv_idx = load_data(
-        [mapfile_path, cv_idx_path], sep=",", header=0, index_col=0, dtype=str
-    )
-
     # load params
     params = load_params(params_filepath)
     train_params = params["train_model"]
-    random_seed, target_size, n_classes, mean_img, std_img = (
+    random_seed, target_size, n_classes, mean_img, std_img, deterministic = (
         params["random_seed"],
         params["target_size"],
         params["n_classes"],
         params["mean_img"],
         params["std_img"],
+        params["deterministic"],
     )
-    batch_size, epochs = train_params["batch_size"], train_params["epochs"]
 
-    # set random seed
-    tf.random.set_seed(random_seed)
     logger.info(
-        f"Debug mode activated. No data augmentation or shuffling"
-    ) if debug else None
+        f"Deterministic mode activated. Data augmentation and shuffle off."
+    ) if deterministic else None
+
+    # read files
+    mapfile_df, cv_idx = load_data(
+        [mapfile_path, cv_idx_path], sep=",", header=0, index_col=0, dtype=str
+    )
 
     # create generator with cv splits
     split_generator = iter(
@@ -77,8 +74,17 @@ def train(
     train_idx, val_idx = next(split_generator)
 
     # create train and validation datasets
-    train_dataset = create_dataset(mapfile_df.iloc[train_idx], params)
-    val_dataset = create_dataset(mapfile_df.iloc[val_idx], params)
+    train_dataset = create_dataset(mapfile_df.iloc[train_idx], params, logger)
+    val_dataset = create_dataset(mapfile_df.iloc[val_idx], params, logger)
+
+    # set batch size, epochs, and steps per epoch
+    batch_size, epochs = train_params["batch_size"], train_params["epochs"]
+    train_steps_per_epoch = np.floor(len(train_idx) / batch_size).astype(np.int)
+    val_steps_per_epoch = np.floor(len(val_idx) / batch_size).astype(np.int)
+
+    # set random seed
+    np.random.seed(random_seed)
+    tf.random.set_seed(random_seed)
 
     # create model
     if params["segmentation"]:
@@ -89,10 +95,9 @@ def train(
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
             metrics=["accuracy"],
         )
-        # print(model.summary())
     else:
         model = networks.simple_nn(
-            input_shape=target_size, n_classes=n_classes, debug=debug
+            input_shape=target_size, n_classes=n_classes, deterministic=deterministic
         )
         model.compile(
             optimizer=tf.keras.optimizers.Adam(0.001),
@@ -110,9 +115,9 @@ def train(
     history = model.fit(
         train_dataset,
         batch_size=batch_size,
-        steps_per_epoch=np.floor(len(train_idx) / batch_size).astype(np.int),
+        steps_per_epoch=train_steps_per_epoch,
         validation_data=val_dataset,
-        validation_steps=np.floor(len(val_idx) / batch_size).astype(np.int),
+        validation_steps=val_steps_per_epoch,
         epochs=epochs,
         callbacks=[callbacks],
         verbose=1,
@@ -122,20 +127,28 @@ def train(
     logger.info(f"Trained for {total_epochs} epochs in {elapsed_time:.4f} seconds")
 
     # update metrics.json
-    metrics_filepath = Path(results_dir).joinpath(f"{metrics_filepath}")
+    results_dir = Path(results_dir).resolve()
+    metrics_filepath = results_dir.joinpath(f"{metrics_file}")
     metrics = {
         "time": elapsed_time,
         "epochs": total_epochs,
+        "iterations": int(total_epochs * train_steps_per_epoch),
         "loss": history.history["loss"][-1],
         "accuracy": history.history["accuracy"][-1],
         "val_loss": history.history["val_loss"][-1],
         "val_accuracy": history.history["val_accuracy"][-1],
         "n_images": mapfile_df.shape[0],
+        "n_train": len(train_idx),
+        "n_val": len(val_idx),
     }
-    save_metrics(metrics, metrics_filepath)
+    save_metrics(metrics, str(metrics_filepath))
 
     # save model
-    model_filename = Path(model_dir).joinpath(f"{model_name}_{total_epochs:03d}")
+    model_dir = Path(model_dir).resolve()
+    model_filename = model_dir.joinpath(f"{model_name}_{total_epochs:03d}")
+    if not model_dir.exists():
+        model_dir.mkdir()
+
     model.save(model_filename, save_format="tf")
 
     return history
@@ -144,9 +157,10 @@ def train(
 def create_dataset(
     mapfile_df,
     params,
-    debug=False,
+    logger,
 ):
-    """ """
+    """Accepts mapfile_df as Pandas DataFrame, dictionary of parameters, and logger and returns
+    a Tensorflow Dataset instance"""
 
     # create dataset using tf.data
     data_records = [list(elem) for elem in mapfile_df.to_records(index=False)]
@@ -154,20 +168,21 @@ def create_dataset(
 
     # set params
     batch_size = params["train_model"]["batch_size"]
-    random_seed, target_size, mean_img, std_img = (
+    random_seed, target_size, mean_img, std_img, deterministic = (
         params["random_seed"],
         params["target_size"],
         params["mean_img"],
         params["std_img"],
+        params["deterministic"],
     )
 
-    # build tf.data pipeline
+    # build tf.data pipeline - map transforms before caching
     if params["segmentation"]:
         dataset = dataset.map(
             tf_imreadpair,
             num_parallel_calls=AUTOTUNE,
         )
-        if not debug:
+        if not deterministic:
             dataset = dataset.map(
                 lambda x, y: apply_transforms_pair(
                     x, y, input_shape=target_size, mean=mean_img, std=std_img
@@ -179,33 +194,31 @@ def create_dataset(
             tf_imread,
             num_parallel_calls=AUTOTUNE,
         )
-        # apply transforms except while debugging
-        if not debug:
+        # apply transforms except under deterministic mode
+        if not deterministic:
             dataset = dataset.map(
                 lambda x, y: apply_transforms(x, y, input_shape=target_size),
                 num_parallel_calls=AUTOTUNE,
             )
 
-    # # apply transforms except while debugging
-    # if not debug:
-    #     dataset = dataset.map(
-    #         lambda x, y: apply_transforms_pair(x, y, input_shape=target_size),
-    #         num_parallel_calls=AUTOTUNE,
-    #     )
-
-    dataset = (
-        dataset.cache()  # cache after mapping
-        .shuffle(  # shuffle after caching to randomize order TODO - turn off with debug
+    # cache before shuffle to randomize order
+    dataset = dataset.cache()
+    if not deterministic:
+        dataset = dataset.shuffle(
             buffer_size=mapfile_df.shape[0],
             seed=random_seed,
             reshuffle_each_iteration=True,
         )
-        .repeat()  # infinite cardinality
+
+    # apply repeat after shuffle to show every example of one epoch before moving to the next
+    # otherwise, a repeat before shuffle will mix epoch boundaries together
+    dataset = (
+        dataset.repeat()  # infinite cardinality
         .batch(
             batch_size=batch_size,
             drop_remainder=True,
             num_parallel_calls=AUTOTUNE,
-            deterministic=debug,
+            deterministic=deterministic,
         )
         .prefetch(AUTOTUNE)
     )
@@ -216,7 +229,7 @@ def create_dataset(
 @click.command()
 @click.argument(
     "mapfile_path",
-    default=Path("./data/interim/mapfile_df.csv").resolve(),
+    default=Path("./data/processed/mapfile_df.csv").resolve(),
     type=click.Path(exists=True),
     # help="Filepath to the CSV with image filenames and class labels.",
 )
@@ -227,42 +240,35 @@ def create_dataset(
     # help="Filepath to the CSV with the cross validation train/dev splits.",
 )
 @click.option("--params_filepath", "-p", default="params.yaml")
-# @click.option(
-#     "--results-dir",
-#     default=Path("./results").resolve(),
-#     type=click.Path(),
-# )
+@click.option(
+    "--results-dir",
+    default=Path("./results").resolve(),
+    type=click.Path(),
+)
 @click.option(
     "--model-dir",
-    default=Path("./models").resolve(),
+    default=Path("./models/dev").resolve(),
     type=click.Path(),
 )
 @click.option(
     "--model-name",
     default="model",
 )
-@click.option(
-    "--debug",
-    is_flag=True,
-    help="Debug switch that turns off augmentation, shuffle, and makes runs deterministic.",
-)
 def main(
     mapfile_path,
     cv_idx_path,
     params_filepath="params.yaml",
-    # results_dir="./results",
+    results_dir="./results",
     model_dir="./models",
     model_name="model",
-    debug=False,
 ):
     train(
         mapfile_path,
         cv_idx_path,
         params_filepath=params_filepath,
-        # results_dir=results_dir,
+        results_dir=results_dir,
         model_dir=model_dir,
         model_name=model_name,
-        debug=debug,
     )
 
 