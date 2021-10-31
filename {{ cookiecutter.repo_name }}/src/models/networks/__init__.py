b0f33b8f5541182adeec0cecb67175f8cd493340
Jeff Nirschl <jnirschl@stanford.edu>
2021-10-31 10:07:40 -0700

    Removed unused import

--- {{ cookiecutter.repo_name }}/src/models/networks/__init__.py
+++ {{ cookiecutter.repo_name }}/src/models/networks/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-__all__ = ["simple_nn", "unet_xception.py", "unet_xception"]
+__all__ = ["simple_nn", "unet_xception"]
 
 from .simple_nn import simple_nn
-from .unet_xception import unet, unet_xception
+from .unet_xception import unet_xception
 
 # from .split import split
