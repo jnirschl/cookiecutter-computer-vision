Fix unet with instance normalization Reformat resnet20.py Add param random_seed to unet_xception.py

{{ cookiecutter.repo_name }}/src/models/networks/resnet20.py
@@ -15,7 +15,7 @@
 
 """TF Keras definition for Resnet-20 for CIFAR."""
 
-from typing import Any, Dict
+from typing import Any, Dict, Iterable
 
 import tensorflow as tf
 
@@ -27,85 +27,88 @@ def _resnet_layer(
     strides: int = 1,
     use_activation: bool = True,
     use_norm: bool = True,
-    l2_weight: float = 1e-4) -> tf.Tensor:
-  """2D Convolution-Batch Normalization-Activation stack builder.
+    l2_weight: float = 1e-4,
+) -> tf.Tensor:
+    """2D Convolution-Batch Normalization-Activation stack builder.
 
-  Args:
-    inputs: input tensor from input image or previous layer.
-    num_filters: Conv2D number of filters.
-    kernel_size: Conv2D square kernel dimensions.
-    strides: Conv2D square stride dimensions.
-    use_activation: whether or not to use a non-linearity.
-    use_norm: whether to include normalization.
-    l2_weight: the L2 regularization coefficient to use for the convolution
-      kernel regularizer.
+    Args:
+      inputs: input tensor from input image or previous layer.
+      num_filters: Conv2D number of filters.
+      kernel_size: Conv2D square kernel dimensions.
+      strides: Conv2D square stride dimensions.
+      use_activation: whether or not to use a non-linearity.
+      use_norm: whether to include normalization.
+      l2_weight: the L2 regularization coefficient to use for the convolution
+        kernel regularizer.
 
-  Returns:
-      Tensor output of this layer.
-  """
-  kernel_regularizer = None
-  if l2_weight:
-    kernel_regularizer = tf.keras.regularizers.l2(l2_weight)
-  conv_layer = tf.keras.layers.Conv2D(
-      num_filters,
-      kernel_size=kernel_size,
-      strides=strides,
-      padding='same',
-      kernel_initializer='he_normal',
-      kernel_regularizer=kernel_regularizer)
+    Returns:
+        Tensor output of this layer.
+    """
+    kernel_regularizer = None
+    if l2_weight:
+        kernel_regularizer = tf.keras.regularizers.l2(l2_weight)
+    conv_layer = tf.keras.layers.Conv2D(
+        num_filters,
+        kernel_size=kernel_size,
+        strides=strides,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=kernel_regularizer,
+    )
 
-  x = conv_layer(inputs)
-  x = tf.keras.layers.BatchNormalization()(x) if use_norm else x
-  x = tf.keras.layers.ReLU()(x) if use_activation is not None else x
-  return x
+    x = conv_layer(inputs)
+    x = tf.keras.layers.BatchNormalization()(x) if use_norm else x
+    x = tf.keras.layers.ReLU()(x) if use_activation is not None else x
+    return x
 
 
 def resnet20(
-    batch_size: int,
+    input_shape: Iterable[int],
+    filters: int = 16,
+    num_classes: int = 10,
+    random_seed: int = 0,
     l2_weight: float = 0.0,
-    **unused_kwargs: Dict[str, Any]) -> tf.keras.models.Model:
-  """Resnet-20 v1, takes (32, 32, 3) input and returns logits of shape (10,)."""
-  # TODO(znado): support NCHW data format.
-  input_layer = tf.keras.layers.Input(
-      shape=(32, 32, 3), batch_size=batch_size)
-  depth = 20
-  num_filters = 16
-  num_res_blocks = int((depth - 2) / 6)
+    batch_size: int = 32,
+    **unused_kwargs: Dict[str, Any]
+) -> tf.keras.models.Model:
+    """Resnet-20 v1, takes tuple of input_shape and returns logits of shape (num_classes,)."""
+    # TODO(znado): support NCHW data format.
+    tf.random.set_seed(random_seed)
+    input_layer = tf.keras.layers.Input(shape=input_shape)  # , batch_size=batch_size
+    depth = 20
+    num_filters = filters
+    num_res_blocks = int((depth - 2) / 6)
 
-  x = _resnet_layer(
-      inputs=input_layer,
-      num_filters=num_filters,
-      l2_weight=l2_weight)
-  for stack in range(3):
-    for res_block in range(num_res_blocks):
-      strides = 1
-      if stack > 0 and res_block == 0:
-        strides = 2
-      y = _resnet_layer(
-          inputs=x,
-          num_filters=num_filters,
-          strides=strides,
-          l2_weight=l2_weight)
-      y = _resnet_layer(
-          inputs=y,
-          num_filters=num_filters,
-          use_activation=False,
-          l2_weight=l2_weight)
-      if stack > 0 and res_block == 0:
-        x = _resnet_layer(
-            inputs=x,
-            num_filters=num_filters,
-            kernel_size=1,
-            strides=strides,
-            use_activation=False,
-            use_norm=False,
-            l2_weight=l2_weight)
-      x = tf.keras.layers.add([x, y])
-      x = tf.keras.layers.ReLU()(x)
-    num_filters *= 2
+    x = _resnet_layer(inputs=input_layer, num_filters=num_filters, l2_weight=l2_weight)
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0:
+                strides = 2
+            y = _resnet_layer(
+                inputs=x, num_filters=num_filters, strides=strides, l2_weight=l2_weight
+            )
+            y = _resnet_layer(
+                inputs=y,
+                num_filters=num_filters,
+                use_activation=False,
+                l2_weight=l2_weight,
+            )
+            if stack > 0 and res_block == 0:
+                x = _resnet_layer(
+                    inputs=x,
+                    num_filters=num_filters,
+                    kernel_size=1,
+                    strides=strides,
+                    use_activation=False,
+                    use_norm=False,
+                    l2_weight=l2_weight,
+                )
+            x = tf.keras.layers.add([x, y])
+            x = tf.keras.layers.ReLU()(x)
+        num_filters *= 2
 
-  x = tf.keras.layers.AveragePooling2D(pool_size=8)(x)
-  x = tf.keras.layers.Flatten()(x)
-  logits = tf.keras.layers.Dense(10, kernel_initializer='he_normal')(x)
-  return tf.keras.models.Model(
-      inputs=input_layer, outputs=logits, name='resnet20')
+    x = tf.keras.layers.AveragePooling2D(pool_size=8)(x)
+    x = tf.keras.layers.Flatten()(x)
+    logits = tf.keras.layers.Dense(num_classes, kernel_initializer="he_normal")(x)
+    return tf.keras.models.Model(inputs=input_layer, outputs=logits, name="resnet20")

{{ cookiecutter.repo_name }}/src/models/networks/unet.py
@@ -20,137 +20,137 @@ import tensorflow as tf
 
 
 class InstanceNormalization(tf.keras.layers.Layer):
-  """Instance Normalization Layer (https://arxiv.org/abs/1607.08022)."""
+    """Instance Normalization Layer (https://arxiv.org/abs/1607.08022)."""
 
-  def __init__(self, epsilon=1e-5):
-    super(InstanceNormalization, self).__init__()
-    self.epsilon = epsilon
+    def __init__(self, epsilon=1e-5):
+        super(InstanceNormalization, self).__init__()
+        self.epsilon = epsilon
 
-  def build(self, input_shape):
-    self.scale = self.add_weight(
-        name='scale',
-        shape=input_shape[-1:],
-        initializer=tf.random_normal_initializer(1., 0.02),
-        trainable=True)
+    def build(self, input_shape):
+        self.scale = self.add_weight(
+            name="scale",
+            shape=input_shape[-1:],
+            initializer=tf.random_normal_initializer(1.0, 0.02),
+            trainable=True,
+        )
 
-    self.offset = self.add_weight(
-        name='offset',
-        shape=input_shape[-1:],
-        initializer='zeros',
-        trainable=True)
+        self.offset = self.add_weight(
+            name="offset", shape=input_shape[-1:], initializer="zeros", trainable=True
+        )
 
-  def call(self, x: tf.Tensor) -> tf.Tensor:
-    mean, variance = tf.nn.moments(x, axes=[1, 2], keepdims=True)
-    inv = tf.math.rsqrt(variance + self.epsilon)
-    normalized = (x - mean) * inv
-    return self.scale * normalized + self.offset
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        mean, variance = tf.nn.moments(x, axes=[1, 2], keepdims=True)
+        inv = tf.math.rsqrt(variance + self.epsilon)
+        normalized = (x - mean) * inv
+        return self.scale * normalized + self.offset
 
 
-def upsample(filters, size, norm_type='batchnorm', apply_dropout=False):
-  """Upsamples an input.
+def upsample(filters, size, norm_type="batchnorm", apply_dropout=False):
+    """Upsamples an input.
 
-  Conv2DTranspose => Batchnorm => Dropout => Relu.
+    Conv2DTranspose => Batchnorm => Dropout => Relu.
 
-  Args:
-    filters: number of filters.
-    size: Filter size.
-    norm_type: Normalization type; either 'batchnorm' or 'instancenorm'.
-    apply_dropout: If True, adds the dropout layer.
+    Args:
+      filters: number of filters.
+      size: Filter size.
+      norm_type: Normalization type; either 'batchnorm' or 'instancenorm'.
+      apply_dropout: If True, adds the dropout layer.
 
-  Returns:
-    Upsample Sequential Model.
-  """
+    Returns:
+      Upsample Sequential Model.
+    """
 
-  initializer = tf.random_normal_initializer(0., 0.02)
+    initializer = tf.random_normal_initializer(0.0, 0.02)
 
-  result = tf.keras.Sequential()
-  result.add(
-      tf.keras.layers.Conv2DTranspose(
-          filters,
-          size,
-          strides=2,
-          padding='same',
-          kernel_initializer=initializer,
-          use_bias=False))
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2DTranspose(
+            filters,
+            size,
+            strides=2,
+            padding="same",
+            kernel_initializer=initializer,
+            use_bias=False,
+        )
+    )
 
-  if norm_type.lower() == 'batchnorm':
-    result.add(tf.keras.layers.BatchNormalization())
-  elif norm_type.lower() == 'instancenorm':
-    result.add(InstanceNormalization())
+    if norm_type.lower() == "batchnorm":
+        result.add(tf.keras.layers.BatchNormalization())
+    elif norm_type.lower() == "instancenorm":
+        result.add(InstanceNormalization())
 
-  if apply_dropout:
-    result.add(tf.keras.layers.Dropout(0.5))
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
 
-  result.add(tf.keras.layers.ReLU())
-  return result
+    result.add(tf.keras.layers.ReLU())
+    return result
 
 
 def unet(
     input_shape: Iterable[int],
-    filters: Iterable[int],
-    num_classes: int = 4,
-    trainable_encoder: bool = False,
-    seed: int = 0,
+    filters: Iterable[int] = [512, 256, 128, 64],
+    num_classes: int = 2,
+    trainable_encoder: bool = True,
+    random_seed: int = 0,
 ) -> tf.keras.models.Model:
-  """Build unet model for segmentation.
+    """Build unet model for segmentation.
 
-  https://www.tensorflow.org/tutorials/images/segmentation
+    https://www.tensorflow.org/tutorials/images/segmentation
 
-  Args:
-    input_shape: tf.Tensor.
-    filters: filters in unet model.
-    num_classes: number of classes.
-    trainable_encoder: whether encoder is trainable.
-    seed: tf.random seed.
+    Args:
+      input_shape: tf.Tensor.
+      filters: filters in unet model.
+      num_classes: number of classes.
+      trainable_encoder: whether encoder is trainable.
+      seed: tf.random seed.
 
-  Returns:
-    tf.keras.Model.
-  """
-  tf.random.set_seed(seed)
-  base_model = tf.keras.applications.MobileNetV2(
-      input_shape=input_shape, include_top=False)
+    Returns:
+      tf.keras.Model.
+    """
+    tf.random.set_seed(random_seed)
+    base_model = tf.keras.applications.MobileNetV2(
+        input_shape=input_shape, include_top=False
+    )
 
-  # Use the activations of these layers
-  layer_names = [
-      'block_1_expand_relu',  # 64x64
-      'block_3_expand_relu',  # 32x32
-      'block_6_expand_relu',  # 16x16
-      'block_13_expand_relu',  # 8x8
-      'block_16_project',  # 4x4
-  ]
-  base_model_outputs = [
-      base_model.get_layer(name).output for name in layer_names
-  ]
+    # Use the activations of these layers
+    layer_names = [
+        "block_1_expand_relu",  # 64x64
+        "block_3_expand_relu",  # 32x32
+        "block_6_expand_relu",  # 16x16
+        "block_13_expand_relu",  # 8x8
+        "block_16_project",  # 4x4
+    ]
+    base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
 
-  # Create the feature extraction model
-  down_stack = tf.keras.Model(
-      inputs=base_model.input, outputs=base_model_outputs)
+    # Create the feature extraction model
+    down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
 
-  down_stack.trainable = trainable_encoder
+    down_stack.trainable = trainable_encoder
 
-  up_stack = []
-  for filter_ in filters:
-    up_stack.append(upsample(filter_, 3))
+    up_stack = []
+    for filter_ in filters:
+        up_stack.append(upsample(filter_, 3))
 
-  def unet_model(output_channels):
-    inputs = tf.keras.layers.Input(shape=input_shape)
+    def unet_model(output_channels):
+        inputs = tf.keras.layers.Input(shape=input_shape)
 
-    # Downsampling through the model
-    skips = down_stack(inputs)
-    x = skips[-1]
-    skips = reversed(skips[:-1])
+        # Downsampling through the model
+        skips = down_stack(inputs)
+        x = skips[-1]
+        skips = reversed(skips[:-1])
 
-    # Upsampling and establishing the skip connections
-    for up, skip in zip(up_stack, skips):
-      x = up(x)
-      concat = tf.keras.layers.Concatenate()
-      x = concat([x, skip])
+        # Upsampling and establishing the skip connections
+        for up, skip in zip(up_stack, skips):
+            x = up(x)
+            concat = tf.keras.layers.Concatenate()
+            x = concat([x, skip])
 
-    # This is the last layer of the model
-    last = tf.keras.layers.Conv2DTranspose(
-        output_channels, 3, strides=2, padding='same')  # 64x64 -> 128x128
+        # This is the last layer of the model
+        last = tf.keras.layers.Conv2DTranspose(
+            output_channels, 3, strides=2, activation="softmax", padding="same"
+        )  # 64x64 -> 128x128
 
-    x = last(x)
-    return tf.keras.Model(inputs=inputs, outputs=x)
+        x = last(x)
+        return tf.keras.Model(inputs=inputs, outputs=x)
 
-  return unet_model(num_classes)
+    return unet_model(num_classes)

{{ cookiecutter.repo_name }}/src/models/networks/unet_xception.py
@@ -4,10 +4,11 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 
-def unet_xception(input_shape, n_classes, padding="same", downsample=2):
+def unet_xception(input_shape, n_classes, padding="same", downsample=2, random_seed=1):
     """
     From https://keras.io/examples/vision/oxford_pets_image_segmentation/#prepare-unet-xceptionstyle-model
     """
+    tf.random.set_seed(random_seed)
     inputs = layers.Input(shape=input_shape)
 
     ### [First half of the network: downsampling inputs] ###

{{ cookiecutter.repo_name }}/src/models/train_model.py
@@ -97,7 +97,11 @@ def train(
 
     # create model
     if params["segmentation"]:
-        model = networks.unet_xception(input_shape=target_size, n_classes=n_classes)
+        model = networks.unet(
+            input_shape=target_size,
+            num_classes=n_classes,
+            random_seed=random_seed,
+        )
         optimizer = tf.keras.optimizers.Adam(0.001)
         model.compile(
             optimizer=optimizer,
@@ -105,8 +109,8 @@ def train(
             metrics=["accuracy"],
         )
     else:
-        model = networks.simple_nn(
-            input_shape=target_size, n_classes=n_classes, deterministic=deterministic
+        model = networks.resnet20(
+            input_shape=target_size, num_classes=n_classes, seed=random_seed
         )
         model.compile(
             optimizer=tf.keras.optimizers.Adam(0.001),
@@ -114,6 +118,7 @@ def train(
             metrics=["accuracy"],
         )
 
+    logger.info(model.summary())
     # set callbacks
     callbacks = train_callbacks.set(params_filepath=params_filepath)
 