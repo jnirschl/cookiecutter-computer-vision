b27bf00280c0aa4b437290eb4f3d45579214ed7f
Jeff Nirschl <jnirschl@stanford.edu>
2021-12-23 07:33:54 -0800

    Remove python2 compatibility. Update setup.py

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -6,5 +6,7 @@ setup(
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
+    python_requires='>=3',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
 )
