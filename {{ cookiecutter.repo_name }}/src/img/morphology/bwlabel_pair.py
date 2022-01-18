Fix bwlabel_pair.py to compute optimal pairing for cost averaging distance and IoU

{{ cookiecutter.repo_name }}/src/img/morphology/bwlabel_pair.py
@@ -10,8 +10,7 @@ cv2.setUseOptimized(True)
 
 
 def bwlabel_pair(gt, mask, conn=8):
-    """Create corresponding label matrices from a pair of ground truth
-    and predicted segmentations"""
+    """Match the label images from a pair of ground truth and predicted masks"""
 
     # get label matrix
     n_gt, label_gt, stats_gt, ctr_gt = cv2.connectedComponentsWithStats(
@@ -21,38 +20,52 @@ def bwlabel_pair(gt, mask, conn=8):
         mask, connectivity=conn
     )
 
-    # compute cost matrix (distance)
+    # compute distance cost  matrix
     dist_cost = distance.cdist(ctr_gt, ctr_pred)
 
-    # # compute cost area (abs(A_gt - A_pred))
-    # area_gt = np.transpose(np.broadcast_to(stats_gt[:,cv2.CC_STAT_AREA],
-    #                                        (num_labels_pred, num_labels_gt)))
-    # area_pred = np.broadcast_to(stats_pred[:,cv2.CC_STAT_AREA],
-    #                             (num_labels_gt, num_labels_pred))
-    # area_diff = np.abs(np.subtract(area_gt, area_pred))
+    # normalize distance cost matrix
+    dist_cost = (dist_cost - dist_cost.min()) / (dist_cost.max() - dist_cost.min())
 
-    # cost IoU
+    # compute intersection over union
+    iou_cost = np.zeros((n_gt, n_pred), dtype=float)
+
+    for m_row in range(n_gt):
+        temp_gt = (label_gt == m_row).astype(np.uint8)
+
+        for n_col in range(n_pred):
+            temp_pred = (label_pred == n_col).astype(np.uint8)
+            temp_intersect = np.sum(cv2.bitwise_and(temp_gt, temp_pred)[:]) + 1e-5
+            temp_union = np.sum(cv2.bitwise_or(temp_gt, temp_pred)[:]) + 1e-5
+            iou_cost[m_row, n_col] = -np.log(temp_intersect / temp_union)
+
+    # normalize IoU matrix
+    iou_cost = np.divide((iou_cost - iou_cost.min()), (iou_cost.max() - iou_cost.min()))
+
+    # combine distance and IoU cost
+    final_cost = np.divide(dist_cost + iou_cost, 2)
 
     # lap
-    row_idx, col_idx = optimize.linear_sum_assignment(dist_cost, maximize=False)
+    row_idx, col_idx = optimize.linear_sum_assignment(final_cost, maximize=False)
 
     # create new output label image
     out_gt = np.zeros_like(label_gt)
     out_pred = np.zeros_like(label_pred)
     for idx, (m_row, n_col) in enumerate(zip(row_idx, col_idx)):
-        out_gt[label_gt == m_row] = idx + 1
-        out_pred[label_pred == n_col] = idx + 1
+        out_gt[label_gt == m_row] = idx
+        out_pred[label_pred == n_col] = idx
 
-    # add gt labels that didn't match
+    row_set = set(row_idx)
     gt_set = set(range(n_gt))
-    unused_gt = gt_set.symmetric_difference(set(row_idx))
-    for idx, m_row in zip(range(len(row_idx) + 1, n_gt + 1), unused_gt):
+    unused_gt = gt_set.symmetric_difference(row_set)
+
+    for idx, m_row in zip(range(len(row_idx), n_gt), unused_gt):
         out_gt[label_gt == m_row] = idx
 
-    # add pred labels that didn't match
+    col_set = set(col_idx)
     pred_set = set(range(n_pred))
-    unused_pred = pred_set.symmetric_difference(set(col_idx))
-    for idx, n_col in zip(range(len(col_idx) + 1, n_pred + 1), unused_pred):
+    unused_pred = pred_set.symmetric_difference(col_set)
+
+    for idx, n_col in zip(range(len(col_idx), n_pred), unused_pred):
         out_pred[label_pred == n_col] = idx
 
     return out_gt, out_pred